92c66125e502167390800e55c913c84c948420b4
DavoSeHe <71927763+DavoSeHe@users.noreply.github.com>
2025-07-01 08:43:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const puerto = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en http://localhost:${puerto}`);
 });
