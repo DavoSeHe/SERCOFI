e9365eca77f3e1c5fc685ddd7632fe98453aa0b2
DavoSeHe <71927763+DavoSeHe@users.noreply.github.com>
2025-07-16 10:41:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const puerto = 3000;
+const puerto = process.env.PORT || 3000;
 
+// Servir archivos estáticos
 app.use(express.static('public'));
 
+// Redireccionar raíz al archivo principal
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Iniciar el servidor
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en http://localhost:${puerto}`);
 });
